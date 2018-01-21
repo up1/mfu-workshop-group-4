667caaef5d2e38521fdd3c6eb11830a698fe2566
MinMin <netchanok0404@gmail.com>
2018-01-21 15:59:25 +0700

    edit

--- src/main/java/api/servlet/ProductServlet.java
+++ src/main/java/api/servlet/ProductServlet.java
@@ -1,7 +1,11 @@
 package api.servlet;
 
 import java.io.IOException;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,21 +17,65 @@ import org.codehaus.jackson.map.ObjectMapper;
 @WebServlet("/product")
 public class ProductServlet extends HttpServlet {
 
+                // JDBC driver name and database URL
+                static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+                static final String DB_URL="jdbc:mysql://localhost/3300/wallet";
+          
+                //  Database credentials
+                static final String USER = "root";
+                static final String PASS = "1234";
+				private Object product;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
+                // String docType =
+                // "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
+                // out.println(docType +
+                // "<html>\n" +
+                // "<head><title>" + productName + "</title></head>\n" +
+                // "<body bgcolor = \"#f0f0f0\">\n" +
+                // "<h1 align = \"center\">" + price + "</h1>\n");
 
-        Product product = new Product();
-        product.setId("0007");
-        product.setProductName("Coco");
-        product.setPrice(55.00);
+                try {
+                    // Register JDBC driver
+                    Class.forName("com.mysql.jdbc.Driver");
+           
+                    // Open a connection
+                    Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+           
+                    // Execute SQL query
+                    Statement stmt = conn.createStatement();
+                    String sql;
+                    sql = "SELECT * FROM wallet.cart";
+                    ResultSet rs = stmt.executeQuery(sql);
+                    // Extract data from result set
+
+         while(rs.next()){
+            //Retrieve by column name
+            Product product = new Product();
+            product.setId(rs.getString("0007"));
+            product.setProductName(rs.getString("Coco"));
+            product.setPrice(rs.getDouble("55.00"));
+                }
 
         ObjectMapper mapper = new ObjectMapper();
         mapper.writeValue(resp.getOutputStream(), product);
 
+        // Clean-up environment
+        rs.close();
+        stmt.close();
+        conn.close();
+     } catch(SQLException se) {
+        //Handle errors for JDBC
+        se.printStackTrace();
+     } catch(Exception e) {
+        //Handle errors for Class.forName
+        e.printStackTrace();
+     } finally {
+        //finally block used to close resources
+
     }
-
-
 }
 
 class Product {
@@ -58,4 +106,5 @@ class Product {
     public void setPrice(Double price){
         this.price = price;
     }
+}
 }
